cc66f64858e2b1be7b9f6280b7aca64c74253735
Menura Adithya <64089619+emiz98@users.noreply.github.com>
2023-03-14 08:28:01 +0530

    Added dependencies for gateway level Oauth2 Security

---
+++ GatewayService/src/main/java/org/emiz/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package org.emiz.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+}
